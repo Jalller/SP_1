845fa0a2b23690daf186acc10ce105dbbf417f44
Jantie <jantielarsen@gmail.com>
2025-02-27 11:19:47 +0100

    dtos

--- src/main/java/app/dtos/MovieDTO.java
+++ src/main/java/app/dtos/MovieDTO.java
@@ -1,5 +1,21 @@
 package app.dtos;
 
-public class MovieDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieDTO {
+    private Long id;
+    private String title;
+    private LocalDate releaseDate;
+    private double rating;
+    private List<String> genres;
+    private List<String> actors;
+    private String director;
 }
---
+++ src/main/java/app/dtos/PersonDTO.java
@@ -0,0 +1,14 @@
+package app.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PersonDTO {
+    private Long id;
+    private String name;
+    private String role;
+}
